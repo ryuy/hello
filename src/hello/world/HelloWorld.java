06cfd70cfc922fd3cf04a60ac5b8563cc56fd5cc
g13935nm <g13935nm@s30427.tsuda.ac.jp>
2015-04-10 11:18:17 +0900

    hello,worldたよーーーーん

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author g13935nm
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        // TODO code application logic here
+    }
+}
